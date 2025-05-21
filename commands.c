c201e801b1bcc56d2b4d07ec9a78ba2052dbc12e
Hakob Aghajanyan <haaghaja@c1r5s1.42yerevan.am>
2025-05-21 19:12:48 +0400

    added ls and pwd

---
+++ commands.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// List directory
+// Command: ls
+// opendir returns something like file descriptor, like open() funciton "open(filename, O_RDONLY)"
+// readdir returns a struct of a file, it working like get_next_line, each time when you call it, it returns a
+// file struct and switch to the next one.
+void	listDir(const char *path)
+{
+	DIR		*dir;
+	struct dirent	*entry;
+
+	dir = opendir(path);
+	if (!dir)
+		return ;
+	entry = readdir(dir);
+	while (entry)
+	{
+		printf("%s", entry->d_name);
+		entry = readdir(dir);
+		if (entry)
+			printf(" ");
+	}
+	printf("\n");
+	closedir(dir);
+}
+
+// Print working directory
+// Command: pwd
+// when getcwd got NULL pointer as first argument, it will allocate memory by it self. We can give our own pointer, 
+// but we can't calculate the size of the path.
+void	pwd()
+{
+	char	*buffer;
+	char	*path;
+
+	buffer = getcwd(NULL, 0);
+	if (buffer)
+	{
+        	printf("%s\n", buffer);
+		free(buffer);
+	}
+	else
+		printf("Error\n");
+}
+
+int	main()
+{
+	listDir(".");
+	pwd();
+	return 0;
+}
